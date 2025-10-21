ec294507d8240ce61fdebe5bca25d79fa21d817f
Luka Bradic <bradicluka04@gmail.com>
2025-10-21 22:35:02 +0200

    [MOD] - Frontend ENV

--- frontend/src/app/environment/environment.ts
+++ frontend/src/app/environment/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   oidc: {
-    authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_byL11guTB',
+    authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_fZa6nkTm2',
     redirectUrl: 'http://localhost:4200/home',
-    clientId: '3jsv5prvcumcami95fbajkvico',
-    logoutUrl: 'https://cloudmusicapp.auth.eu-central-1.amazoncognito.com/logout?client_id=3jsv5prvcumcami95fbajkvico&logout_uri=http://localhost:4200/home',
+    clientId: '7jhcsh6fia0125035e4chc1gc',
+    logoutUrl: 'https://cloudmusicapp.auth.eu-central-1.amazoncognito.com/logout?client_id=7jhcsh6fia0125035e4chc1gc&logout_uri=http://localhost:4200/home',
   },
-  apiUrl: 'https://q2fyxa1q8g.execute-api.eu-central-1.amazonaws.com/dev/'
+  apiUrl: 'https://g945mglyn5.execute-api.eu-central-1.amazonaws.com/dev'
 };
