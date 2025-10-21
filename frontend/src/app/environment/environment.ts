02b301e25ce687f7092f24898e138e6884a4d130
Luka Bradic <bradicluka04@gmail.com>
2025-10-21 15:09:45 +0200

    [FIX] - urls

--- frontend/src/app/environment/environment.ts
+++ frontend/src/app/environment/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
   oidc: {
-    authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_oMRl9AAzL',
+    authority: 'https://cognito-idp.eu-central-1.amazonaws.com/eu-central-1_byL11guTB',
     redirectUrl: 'http://localhost:4200/home',
-    clientId: '3c7lu14e4gopdbb2vsqscrgarc',
-    logoutUrl: 'https://cloudmusicapp.auth.eu-central-1.amazoncognito.com/logout?client_id=3c7lu14e4gopdbb2vsqscrgarc&logout_uri=http://localhost:4200/home',
+    clientId: '3jsv5prvcumcami95fbajkvico',
+    logoutUrl: 'https://cloudmusicapp.auth.eu-central-1.amazoncognito.com/logout?client_id=3jsv5prvcumcami95fbajkvico&logout_uri=http://localhost:4200/home',
   },
-  apiUrl: 'https://r4riagry0h.execute-api.eu-central-1.amazonaws.com/dev'
+  apiUrl: 'https://q2fyxa1q8g.execute-api.eu-central-1.amazonaws.com/dev/'
 };
